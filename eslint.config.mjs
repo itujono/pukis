ab3e41818280e68c9b1dedf825f40432274f62b6
itujono <itujono@gmail.com>
2024-06-29 18:03:01 +0700

    Okay with fixed lint and format.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,5 @@
 import tsParser from "@typescript-eslint/parser";
-import espree from "espree";
+import * as espree from "espree";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
@@ -8,26 +8,26 @@ import { FlatCompat } from "@eslint/eslintrc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-export default [...compat.extends("prettier"), {
-    rules: {
-        "tailwindcss/no-custom-classname": "off",
-        "tailwindcss/classnames-order": "error",
-    },
-}, {
+export default [
+  ...compat.extends("prettier"),
+  {},
+  {
     files: ["**/*.ts", "**/*.tsx"],
 
     languageOptions: {
-        parser: tsParser,
+      parser: tsParser,
     },
-}, {
+  },
+  {
     files: ["**/*.js", "**/*.jsx"],
 
     languageOptions: {
-        parser: espree,
+      parser: espree,
     },
-}];
+  },
+];
