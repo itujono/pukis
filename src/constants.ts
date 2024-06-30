f2e2aabc859b2b68f2774a1d4108b1e6b1e75407
itujono <itujono@gmail.com>
2024-06-30 18:09:14 +0700

    Change description [MINOR].

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,7 @@
 import { name } from "../package.json";
 
 export const PROGRAM_NAME = name;
+export const PROGRAM_DESCRIPTION = "A CLI for scaffolding components in React";
 
 export const PackageManager = {
   NAME: "pnpm",
--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,14 @@
 
 import { Command } from "commander";
 import { scaffoldComponent } from "./scaffold";
-import { PROGRAM_NAME } from "./constants";
+import { PROGRAM_NAME, PROGRAM_DESCRIPTION } from "./constants";
 import { version } from "../package.json";
 import { initApp } from "./init";
 import { colorize } from "./utils/colorize-log";
 
 const program = new Command();
 
-program.name(PROGRAM_NAME).description("CLI to scaffold React components").version(version);
+program.name(PROGRAM_NAME).description(PROGRAM_DESCRIPTION).version(version);
 
 program.command("init").description("Initialize the project").action(initApp);
 
